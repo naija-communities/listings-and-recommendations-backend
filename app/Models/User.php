188cbb8fae168decb51f3a09f438fdbcd9764b42
fokosun <okosunuzflorence@gmail.com>
2021-01-24 00:39:22 -0700

    set fillable properties

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use HasFactory, Notifiable;
 
@@ -20,6 +20,15 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'pronouns',
+        'relationship_status',
+        'profession',
+        'province',
+        'city',
+        'postal_code',
+        'year_of_entry',
+        'nigerian_identity',
+        'gender'
     ];
 
     /**
@@ -40,4 +49,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function hasVerifiedEmail() {}
+    public function markEmailAsVerified() {}
+    public function sendEmailVerificationNotification() {}
+    public function getEmailForVerification() {}
 }
