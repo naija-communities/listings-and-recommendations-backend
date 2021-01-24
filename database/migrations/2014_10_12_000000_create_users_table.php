13ba32965c33967bb989b66e891ad14373c755c9
fokosun <okosunuzflorence@gmail.com>
2021-01-24 00:39:02 -0700

    user table schema

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,76 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('pronouns', ['she/her', 'he/him'. 'they/them', 'rather not say']);
+            $table->enum('relationship_status', ['single', 'very single', 'taken']);
+            $table->enum('profession', [
+                'accountant',
+                'architect',
+                'banker',
+                'civil engineer',
+                'forex trader',
+                'family doctor',
+                'farmer',
+                'health care professional',
+                'medical doctor',
+                'nurse',
+                'social worker',
+                'network engineer',
+                'software engineer (web)',
+                'software engineer (mobile)',
+                'ui/ux designer',
+                'project management',
+                'student',
+                'tutor/educator',
+                'real estate agent',
+                'retail business owner',
+                'small business owner',
+                'lawyer',
+                'immigration lawyer',
+                'military',
+                'military veteran',
+                'religious leader',
+                'writer',
+                'other',
+                'rather not say'
+            ]);
+            $table->enum('province', [
+                'Alberta AB',
+                'British Columbia BC',
+                'Manitoba MAN',
+                'Newfoundland and Labrador NFL',
+                'Nova Scotia NS',
+                'New Brunswick NB',
+                'Nunavut NVT',
+                'Northwest Territories NWT',
+                'Ontario ON',
+                'Prince Edward Island PEI',
+                'Quebec QUE',
+                'Saskatchewan SASK',
+                'Yukon YT'
+            ]);
+            $table->string('city');
+            $table->string('postal_code');
+            $table->date('year_of_entry');
+            $table->enum('nigerian_identity', [
+                'igbo',
+                'yoruba',
+                'hausa',
+                'port harcourt',
+                'calabar',
+                'warri',
+                'niger delta',
+                'edo',
+                'nigerian'
+            ])->nullable();
+            $table->enum('gender', [
+                'man',
+                'woman',
+                'trans man',
+                'trans woman',
+                'transgender',
+                'rather not say'
+            ])->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
